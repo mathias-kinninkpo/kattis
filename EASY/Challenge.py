be2bb6a451a429a9306276b1cecdf9ba9b1035d3
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-08-22 01:03:32 +0100

    ajout de fichier

---
+++ EASY/Challenge.py
@@ -0,0 +1,15 @@
+def move(p,pos):
+    if p == 'u':
+        pos['y'] += 1
+    if p == 'd':
+        pass
+    if p == 'r':
+        pass
+    if p == 'l':
+        pass
+pos = {'x':0, 'y':0}       
+w,l = map(int, input().split())
+while w!=0 and l!=0:
+    n = int(input())
+    for _ in range(n):
+        pass
