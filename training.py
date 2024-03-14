7542d1af54e3d716e55a440b17788f7af630fba0
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-03-14 23:07:38 +0000

    training session

---
+++ training.py
@@ -0,0 +1,15 @@
+height = 1.70
+weight = 58.5
+print(height)
+print(weight)
+
+bmi = weight / height ** 2
+
+print(bmi)
+
+print(type(bmi))
+number_of_persons = 4
+print(type(number_of_persons))
+name = "Mathias KINNINKPO"
+print(type(weight))
+print(type(name))
